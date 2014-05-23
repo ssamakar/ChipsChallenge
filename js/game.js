3d41b9ba1dd47a5e364d9f6e9e93a53139a92c8b
Saba Samakar <saba.samakar@gmail.com>
2014-05-22 21:30:44 -0600

    All progress so far on collisions.

--- js/game.js
+++ js/game.js
@@ -19,20 +19,21 @@ function create() {
 	//this loads a physics system into the game.
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
+	//Adding a map with two layers: lots of ground tiles we can walk on, 
+	//and some wall tiles that we won't be able to walk on.
 	map = game.add.tilemap('map');
 	map.addTilesetImage('tileset','tiles');
-	layer = map.createLayer('Tile Layer 1');
-	walls = map.createLayer('Tile Layer 2');
-	walls.enableBody = true;
-	// walls.body.immovable = true;
-	//a group of walls we can collide with.
-	platforms = game.add.group();
-	platforms.enableBody = true;
+	map.createLayer('Tile Layer 1');
+	wall = map.createLayer('Tile Layer 2');
 
-	//adding the character sprite and 
-	player = game.add.sprite(0,0, 'chip');
+	//The player will collide with the tile at index 11 on Tile Layer 2.
+	map.setCollision(11, true, 'Tile Layer 2');
+
+	//adding the character sprite.
+	player = game.add.sprite(64,64, 'chip');
 	game.physics.arcade.enable(player);
 
+	//Chip won't be able to leave the bounds of the game world.
 	player.body.collideWorldBounds = true;
 
 	//Animations for walking. We define em here but we don't call em till later.
@@ -47,15 +48,16 @@ function create() {
 
 function update() {
 
-
-
-	//check for collisions
-	game.physics.arcade.collide(player, platforms);
-	game.physics.arcade.collide(player, walls);
-
-
+	//trying to test collisions here...
+	// if (cursors.right.isDown){
+	// 	player.body.velocity.x += 8;
+	// 	console.log(game.physics.arcade.collide(player, wall))
+	// }
 	//moves chip
 	move(player);
+
+	//check for collisions
+	game.physics.arcade.collide(player, wall)
 
 }
 
@@ -66,15 +68,15 @@ function move(player) {
 			player.body.position.x -= 32;
 			timeCheck = game.time.now;
 		} else if (cursors.right.isDown){
-			player.body.position.x += 32;
+			for (var i = 0; i<4; i++){
+			player.body.position.x += 8;
+		}
 			timeCheck = game.time.now;
 		} else if (cursors.up.isDown){
 			player.body.position.y -= 32;
 			timeCheck = game.time.now;
 		} else if (cursors.down.isDown){
 			player.body.position.y += 32;
-			timeCheck = game.time.now;
-		}
-
+			timeCheck = game.time.now;}
 	}
 }
